e7fdae490e52005f7853b7462f653c54bdb08d99
dansand71 <dansand71@hotmail.com>
2017-04-21 14:35:00 -0400

    fixes for server name

--- 1-setup-demo.sh
+++ 1-setup-demo.sh
@@ -8,15 +8,17 @@ RED="\033[0;31m"
 echo -e "${BOLD}Create Drupal Storage Account?...${RESET}"
 read -p "$(echo -e -n "${INPUT}Create new Storage Account for Drupal Persistent Shares? [Y/n]:"${RESET})" continuescript
 if [[ ${continuescript,,} != "n" ]]; then
-    ~/bin/az storage account create -n VALUEOF-UNIQUE-STORAGE-PREFIXdrupalstore -g ossdemo-appdev-paas -l eastus --sku Standard_LRS
-    
-
+    ~/bin/az storage account create -n VALUEOF-UNIQUE-SERVER-PREFIXdrupalstore -g ossdemo-appdev-paas -l eastus --sku Standard_LRS
 fi
-echo ".getting storage account connection string for VALUEOF-UNIQUE-STORAGE-PREFIXdrupalstore"
-STORAGECONN=`~/bin/az storage account show-connection-string -n VALUEOF-UNIQUE-STORAGE-PREFIXdrupalstore -g ossdemo-appdev-paas`
+echo ".getting storage account connection string for VALUEOF-UNIQUE-SERVER-PREFIXdrupalstore"
+STORAGECONN=`~/bin/az storage account show-connection-string -n VALUEOF-UNIQUE-SERVER-PREFIXdrupalstore -g ossdemo-appdev-paas`
 echo ".found ${STORAGECONN}"
 echo ".creating shares"
 ~/bin/az storage share create --name drupal-sites --connection-string ${STORAGECONN} --quota 100
+~/bin/az storage share create --name drupal-themes --connection-string ${STORAGECONN} --quota 100
+~/bin/az storage share create --name drupal-modules --connection-string ${STORAGECONN} --quota 100
+~/bin/az storage share create --name drupal-profiles --connection-string ${STORAGECONN} --quota 100
+
 echo ".creating mount point"
 #Create Mount Point
 ../environment/create-mount-point.sh
@@ -24,10 +26,11 @@ echo ".copying assets into storage file shares for initial setup"
 #copy files into shares
 cp -r ../vm-assets/sites /mnt/drupal-sites
 echo "-----------------------------"
+
 echo ".base64 encoding Storage Account name and Key"
 #tell kubernetes about the secret with base64 incoding
-B64STORAGENAME=`echo "VALUEOF-UNIQUE-STORAGE-PREFIXdrupalstore" | base64`
-B64STORAGEKEY=`~/bin/az storage account keys list -n VALUEOF-UNIQUE-STORAGE-PREFIXdrupalstore -g ossdemo-appdev-paas --query [1].value -o tsv | base64`
+B64STORAGENAME=`echo "VALUEOF-UNIQUE-SERVER-PREFIXdrupalstore" | base64`
+B64STORAGEKEY=`~/bin/az storage account keys list -n VALUEOF-UNIQUE-SERVER-PREFIXdrupalstore -g ossdemo-appdev-paas --query [1].value -o tsv | base64`
 
 #SED the secret file
 echo ".replacing data in the K8S secrets file for deployment"
--- environment/create-mount-point.sh
+++ environment/create-mount-point.sh
@@ -1,4 +1,5 @@
 echo "Create mount directory:/mnt/drupal-sites"
 sudo mkdir -p /mnt/drupal-sites
 echo "Create  mount point."
-sudo mount -t cifs //REPLACE-DRUPAL-STORAGE-ACCOUNT-NAME.file.core.windows.net/drupal-sites /mnt/drupal-sites -o vers=3.0,username=REPLACE-DRUPAL-STORAGE-ACCOUNT-NAME,password=REPLACE-DRUPAL-STORAGE-KEY,dir_mode=0777,file_mode=0777
+DRUPALSTORAGEKEY=`~/bin/az storage account keys list -n VALUEOF-UNIQUE-SERVER-PREFIXdrupalstore -g ossdemo-appdev-paas --query [1].value -o tsv`
+sudo mount -t cifs //VALUEOF-UNIQUE-SERVER-PREFIXdrupalstore.file.core.windows.net/drupal-sites /mnt/drupal-sites -o vers=3.0,username=VALUEOF-UNIQUE-SERVER-PREFIXdrupalstore,password=${DRUPALSTORAGEKEY},dir_mode=0777,file_mode=0777
